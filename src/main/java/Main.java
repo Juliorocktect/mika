5ca90ff84c231c7ef39b37ae4c217d1f7da94ab9
Juliorocktect <113830349+Juliorocktect@users.noreply.github.com>
2023-06-28 11:22:12 +0200

    added comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,6 @@
 public class Main {
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+    //String args sind die Argumente die das Programm aufnehemen kann beim ausführen
 }
--- src/main/java/Mika.java
+++ src/main/java/Mika.java
@@ -1,3 +1,15 @@
-public class Mika {
-    // konstruktor
+public class Mika { // eine Klasse
+
+    private int age;
+    // private heißt dass, dass nur innerhalb dieser Klasse auf diesen wert zugegriffen werden,
+    // kann ohne Schnittstelle
+    private String name;
+    public Mika(){} //ein Konstruktor setzt die Attribute eines Objektes einem Wert
+
+    public String getName(){return name;}
+    //get Methode
+    // die Schnittstelle
+    private void setName(String name){this.name = name;}
+    // void heißt, dass die Methode keinen Rückgabewert hat
+    //mit this wird auf das Aktuelle Objekt der Klasse gezeigt
 }
