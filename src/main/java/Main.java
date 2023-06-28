67a26397119a73b3a9c59a515e770999c28c0bba
juli <julius.k@upsala>
2023-06-28 15:26:20 +0200

    added methods in mika.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String args[] ){
-        int one;
-        Mika two;
-        Mika third;
-        third = new Mika("mika");
-        System.out.println(third.getName());
+        Mika mika = new Mika("mika");
+        mika.trinken();
     }
 }
--- src/main/java/Mika.java
+++ src/main/java/Mika.java
@@ -1,12 +1,41 @@
 public class Mika {
     private String name;
+    private int money;
 
     public Mika(String name){
         this.name = name;
+        this.money = 100;
     }
 
     public void setName(String name){
         this.name = name;
     }
     public String getName(){return name;}
+
+    //trinken
+    public int lengthOfName()
+    {
+        int length;
+        length = name.length();
+        return length;
+    }
+    public void trinken()
+    {
+      if (isMoneyBiggerThanZeroAndSmallerThen100(money)){
+          money = 0;
+      }
+      else{
+          this.money--;
+      }
+    }
+    public int getMoney(){
+        return money;
+    }
+
+    public boolean isMoneyBiggerThanZeroAndSmallerThen100(int number){
+        if (number > 0 && number < 100){
+            return true;
+        }
+        return false;
+    }
 }
